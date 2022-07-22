7a3d9607ba1e7da90a10d65d429275188cbb36d3
arthur3133 <alanprajapati8624@gmail.com>
2022-07-22 10:58:44 +0530

    Design Homescreen

--- app/src/main/java/com/udemycourse/movieapp/MainActivity.kt
+++ app/src/main/java/com/udemycourse/movieapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.udemycourse.movieapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.udemycourse.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +28,109 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppTheme {
+                HomeScreen()
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                backgroundColor = MaterialTheme.colors.primary,
+                elevation = AppBarDefaults.TopAppBarElevation
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(start = 5.dp)
+                ) {
+                    Text(
+                        text = "Movies",
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+
+            }
+        }
+    ) {
+        Content()
+    }
+}
+
+@Composable
+fun Content(
+    moviesList: List<String>
+    = listOf(
+        "Avenger: End Game",
+        "Thor",
+        "Black Widow",
+        "Doctor Strange",
+        "Spider Man",
+        "Avatar",
+        "Superman",
+        "Justice League",
+        "Batman",
+        "Game Of Throne",
+        "13 Reason Why",
+        "Stranger Things",
+        "Sex Education"
+    )
+) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        LazyColumn {
+            items(moviesList) { movieName ->
+                MovieRow(
+                    movieName = movieName,
+                    onClicked = {
+
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(
+    movieName: String,
+    onClicked: (String) -> Unit
+) {
+   Row(
+       modifier = Modifier
+           .fillMaxWidth()
+           .padding(10.dp)
+           .background(MaterialTheme.colors.background)
+           .clickable {
+               onClicked(movieName)
+           },
+       horizontalArrangement = Arrangement.Start,
+       verticalAlignment = Alignment.CenterVertically
+   ) {
+       Card(
+           modifier = Modifier
+               .size(120.dp)
+               .padding(10.dp),
+           elevation = 4.dp,
+           shape = RoundedCornerShape(4.dp)
+       ) {
+           Image(
+               imageVector = Icons.Default.AccountBox,
+               contentDescription = "Movie Image"
+           )
+       }
+       Spacer(modifier = Modifier.width(20.dp))
+       Text(
+           text = movieName,
+           style = MaterialTheme.typography.h6
+       )
+   }
+    Divider()
 }
